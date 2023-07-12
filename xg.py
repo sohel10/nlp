d634a1d837ba525fb8ea03e9c3f91fbc08ca68b4
Sohel Ahmed <51312348+sohel10@users.noreply.github.com>
2023-07-12 17:03:26 -0400

    Add files via upload

--- xg.py
+++ xg.py
@@ -8,23 +8,12 @@ import streamlit as st
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from PIL import Image
-
 # Load the saved model
-try:
-    with open("xgboost_model.pkl", "rb") as file:
-        model = pickle.load(file)
-except FileNotFoundError:
-    st.error("Model file not found. Please make sure 'xgboost_model.pkl' is in the same directory.")
-    st.stop()
-
+with open("xgboost_model.pkl", "rb") as file:
+    model = pickle.load(file)
 # Load the saved TfidfVectorizer
-try:
-    with open("tfidf_vectorizer1.pkl", "rb") as file:
-        vectorizer1 = pickle.load(file)
-except FileNotFoundError:
-    st.error("Vectorizer file not found. Please make sure 'tfidf_vectorizer1.pkl' is in the same directory.")
-    st.stop()
-
+with open("tfidf_vectorizer1.pkl", "rb") as file:
+    vectorizer = pickle.load(file)
 # Streamlit app code
 st.title("Twitter Speech Analysis App")
 st.markdown("<span style='font-size: 20px;'><strong>By Sohel Ahmed</strong></span>", unsafe_allow_html=True)
@@ -33,16 +22,16 @@ st.image(image, use_column_width=True)
 
 st.subheader("Enter your text here:")
 user_input = st.text_area("")
-
 # Create a predict button
 if st.button("Predict"):
     # Preprocess the input text
-    text_vectorized = vectorizer1.transform([user_input])
+    text_vectorized = vectorizer.transform([user_input])
     # Make predictions
     prediction = model.predict(text_vectorized)[0]
     st.header("Prediction:")
     # Display the predicted sentiment
     if prediction == 'Harm':
-        st.subheader("The sentiment of the given text is: Harm")
+        st.subheader("The sentiment of given text is: Harm")
     elif prediction == 'Not Harm':
-        st.subheader("The sentiment of the given text is: Not Harm")
+        st.subheader("The sentiment of given text is: Not Harm")
+
